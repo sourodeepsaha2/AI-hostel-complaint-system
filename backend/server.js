d734bd6451904b617f4efec1d8cd5a27c5b4373e
Sourodeep Saha <sourodeepsaha28@gmail.com>
2026-06-01 22:03:44 +0530

    Connect backend to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+// Connect Database
+connectDB();
 
 const app = express();
 
@@ -13,10 +19,8 @@ app.get("/", (req, res) => {
   res.send("Backend server is running");
 });
 
-// Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-// Start Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
